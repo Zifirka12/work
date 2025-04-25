7a8b53405b1a49bab62b2c56872d84324aa136e7
Student <Ilaafanasef21@gmail.com>
2025-04-25 15:41:15 +0300

    курсавая исправления

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ['id', 'email', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
